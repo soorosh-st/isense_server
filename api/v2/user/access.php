1f801a6bce0ddfc441d3a21156de5e7094758919
soorosh-st <soorosh.st@gmail.com>
2024-07-05 18:19:43 +0330

    Update access.php

--- api/v2/user/access.php
+++ api/v2/user/access.php
@@ -9,7 +9,27 @@ $data = json_decode(file_get_contents("php://input"));
 $username = filter_var($data->username, FILTER_SANITIZE_STRING);
 $token = filter_var($data->token, FILTER_SANITIZE_STRING);
 
-if (!$username || !$token) {
+
+if (isset($_SERVER['HTTP_AUTHORIZATION'])) {
+    $authHeader = $_SERVER['HTTP_AUTHORIZATION'];
+    list($type, $token) = explode(' ', $authHeader);
+
+    if ($type === 'Bearer' && in_array($token, $validTokens)) {
+        // Token is valid
+        echo json_encode(['data' => 'This is protected data.']);
+    } else {
+        // Invalid token
+        http_response_code(401);
+        echo json_encode(['error' => 'Invalid token']);
+    }
+} else {
+    // Missing token
+    http_response_code(401);
+    echo json_encode(['error' => 'Missing token']);
+}
+
+
+if (!$username) {
     http_response_code(400);
     echo json_encode(array("message" => "Not enough information"));
     die();
