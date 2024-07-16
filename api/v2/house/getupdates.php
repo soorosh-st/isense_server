907b1efe3e45a54ef5e4ef0e535da7aaaeb1009b
soorosh-st <soorosh.st@gmail.com>
2024-07-16 19:20:10 +0330

    Update getupdates.php

--- api/v2/house/getupdates.php
+++ api/v2/house/getupdates.php
@@ -10,15 +10,11 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/config/init.php';
 $data = json_decode(file_get_contents("php://input"));
 
 $id = $data->house_id;
-$from = $data->from;
+
 $house = new house(NULL, $conn, $id);
 
-if ($from == "gw") {
-    $result = $house->getupdates();
-    echo json_encode($result);
-} else {
-
-}
+$result = $house->getupdates();
+echo json_encode($result);
 
 http_response_code(200);
 
