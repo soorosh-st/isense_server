6c54dfea9d7806e1e1ee283ab15fd51e2fc181da
soorosh-st <soorosh.st@gmail.com>
2024-08-03 12:28:05 +0330

    Update create.php

--- api/v2/group/create.php
+++ api/v2/group/create.php
@@ -10,23 +10,23 @@ $title = filter_var($data->title, FILTER_SANITIZE_STRING);
 //$src = filter_var($data->img_src, FILTER_SANITIZE_STRING);
 $house_id = filter_var($data->house_id, FILTER_SANITIZE_STRING);
 
-$token;
-$headers = getallheaders();
-if ($headers['Authorization']) {
+// $token;
+// $headers = getallheaders();
+// if ($headers['Authorization']) {
 
-    $authHeader = $headers['Authorization'];
-    list($type, $token) = explode(' ', $authHeader);
-    if ($type != 'Bearer') {
-        http_response_code(401);
-        echo json_encode(['Message' => 'Invalid token']);
-        die();
-    }
-} else {
-    // Missing token
-    http_response_code(401);
-    echo json_encode(['Message' => 'Missing token']);
-    die();
-}
+//     $authHeader = $headers['Authorization'];
+//     list($type, $token) = explode(' ', $authHeader);
+//     if ($type != 'Bearer') {
+//         http_response_code(401);
+//         echo json_encode(['Message' => 'Invalid token']);
+//         die();
+//     }
+// } else {
+//     // Missing token
+//     http_response_code(401);
+//     echo json_encode(['Message' => 'Missing token']);
+//     die();
+// }
 
 $group = new group(NULL, $title, NULL, $house_id, $conn);
 if ($group->create()) {
