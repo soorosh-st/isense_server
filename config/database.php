4ea17341d706823d11aca74fcdf7432545455221
soorosh-st <soorosh.st@gmail.com>
2024-08-19 22:40:23 +0330

    Update database.php

--- config/database.php
+++ config/database.php
@@ -5,13 +5,14 @@ class database
     private $username = "root";
     private $password = "BLdrnljYG5OHRUPEBLo4EtlU";
     private $databasename = "isense";
+    private $port = 3306;
     private $conn;
 
     public function get_connection()
     {
         $this->conn = null;
         try {
-            $this->conn = new mysqli($this->hostname, $this->username, $this->password, $this->databasename);
+            $this->conn = new mysqli($this->hostname, $this->username, $this->password, $this->databasename, $this->port);
             mysqli_set_charset($this->conn, "utf8");
         } catch (PDOException $e) {
             throw new Exception("connection error" . $e->getMessage(), $e->getCode());
