7cd9c8d6fc7c346077fd75458b5fd579e4dcd70e
soorosh-st <soorosh.st@gmail.com>
2024-08-20 19:42:20 +0330

    comiit

--- config/init.php
+++ config/init.php
@@ -2,7 +2,9 @@
 error_reporting(E_ERROR | E_PARSE);
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config/database.php';
 require_once $_SERVER['DOCUMENT_ROOT'] . '/data/log.php';
+header('Access-Control-Allow-Methods: PUT, GET, POST');
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Cache-Control, Pragma, Authorization, Accept, Accept-Encoding");
 try {
     $database = new database();
     $conn = $database->get_Connection();
