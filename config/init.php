b074d21b7964161d8cb4ddf9ee3d5ef1bc98f8d1
soorosh-st <soorosh.st@gmail.com>
2024-08-24 10:22:15 +0330

    Update init.php

--- config/init.php
+++ config/init.php
@@ -12,15 +12,14 @@ function sendCORSHeaders()
         // you want to allow, and if so:
         header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
         header('Access-Control-Allow-Credentials: true');
-        //header('Access-Control-Max-Age: 86400');    // cache for 1 day
     }
 
 
     if (!empty($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-
+        header('Access-Control-Max-Age: 86400');
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_METHOD']))
             // may also be using PUT, PATCH, HEAD etc
-            header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+            header("Access-Control-Allow-Methods: GET, POST, PUT");
 
         if (isset($_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']))
             header("Access-Control-Allow-Headers: {$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
